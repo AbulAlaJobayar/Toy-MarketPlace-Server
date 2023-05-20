8b5a23f7f7a852ad8418038c1b5ec0ee9a44170c
abul-ala-jobayar-1 <jobayar59@gmail.com>
2023-05-20 13:40:24 +0600

    post-data-send

--- index.js
+++ index.js
@@ -25,6 +25,29 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const toyCollection = client.db("assingment11").collection("hometoy");
+    const dataCollection = client.db("assingment11").collection("alldata");
+
+
+
+
+
+   
+
+app.get('/homedata', async(req,res)=>{
+const result= await toyCollection.find().toArray();
+res.send(result)
+});
+
+
+app.post('/postdata', async(req,res)=>{
+  const  body= req.body;
+  console.log(body);
+  const result =await dataCollection.insertOne(body);
+  res.send(result)
+})
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
